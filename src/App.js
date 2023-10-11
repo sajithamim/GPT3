ab4fece6807bdb6a7d15cbadfb25cab32b5645b4
sajithasoman <sajitha@sayonetech.com>
2023-10-11 17:41:53 +0530

    GPT3 initial set up, file and folder structure, Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+import { Article, Brand, Cta, Feature, Navbar } from "./components/index";
+import {
+  Footer,
+  Blog,
+  Features,
+  Header,
+  WhatGPT3,
+  Possibility,
+} from "./containers/index";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;

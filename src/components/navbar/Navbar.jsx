2f13b406a6c5cca57c5b7a64f2c8f3b3b2c592d9
sajithasoman <sajitha@sayonetech.com>
2023-10-12 18:33:23 +0530

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+            <a href="#wgpt3">What is GPT3</a>
+            <a href="#possibility">Open AI</a>
+            <a href="#features">Case Studies</a>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

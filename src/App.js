272bc69e3c420f11ec17389c6aca5ce21e7b9147
sajithasoman <sajitha@sayonetech.com>
2023-10-15 15:17:24 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import "./App.css";
-import { Article, Brand, Cta, Feature, Navbar } from "./components/index";
-import {
-  Footer,
-  Blog,
-  Features,
-  Header,
-  WhatGPT3,
-  Possibility,
-} from "./containers/index";
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient_bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <Cta />
-      <Blog />
-      <Footer />
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
